f221a4d480c1785680052dddd8e513da90577077
comarley <35406262+comarley@users.noreply.github.com>
2018-03-11 22:47:47 -0400

    Add files via upload

---
+++ ex4/application/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21711/cit313/test/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'comarley');
+define('DB_PASS', 'comarley');
+define('DB_NAME', 'comarley_db');
+
+define('REQFIELD', '<font color="#FF0000">*</font>');
+?>
---
+++ ex4/application/controllers/logincontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+class LoginController extends Controller{
+
+protected $userObject;
+
+   public function do_login() {
+	   // handles login
+
+		 $this->userObject = new Users();
+
+		 if($this->userObject->checkUser($_POST['email'],$_POST['password'])) {
+
+			 $userInfo = $this->userObject->getUserFromEmail($_POST['email']);
+
+			 $_SESSION['uID'] = $userInfo['uID'];
+
+			 header('Location: '.BASE_URL);
+
+			 echo $_SESSION['uID'];
+		 }
+
+		 else {
+			 echo "bad user";
+		 }
+   }
+
+}
